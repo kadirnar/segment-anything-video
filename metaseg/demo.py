94867e827a4eda538b4a34a7cd536aae09144940
kadirnar <kadir.nar@hotmail.com>
2023-04-09 02:48:24 +0300

    min_area variable updated.

--- metaseg/demo.py
+++ metaseg/demo.py
@@ -12,25 +12,24 @@ class SegAutoMaskGenerator:
 
     def load_model(self, model_type):
         if self.model is None:
-            model_path = download_model(model_type)
-            model = sam_model_registry[model_type](checkpoint=model_path)
-            model.to(device=self.device)
-            self.model = model
+            self.model_path = download_model(model_type)
+            self.model = sam_model_registry[model_type](checkpoint=self.model_path)
+            self.model.to(device=self.device)
 
         return self.model
 
-    def predict(self, frame, model_type, points_per_side, points_per_batch):
+    def predict(self, frame, model_type, points_per_side, points_per_batch, min_area):
         model = self.load_model(model_type)
         mask_generator = SamAutomaticMaskGenerator(
-            model, points_per_side=points_per_side, points_per_batch=points_per_batch
-        )
+            model, points_per_side=points_per_side, points_per_batch=points_per_batch, min_mask_region_area=min_area)
+        
         masks = mask_generator.generate(frame)
 
         return frame, masks
 
-    def save_image(self, source, model_type, points_per_side, points_per_batch):
+    def save_image(self, source, model_type, points_per_side, points_per_batch, min_area):
         read_image = load_image(source)
-        image, anns = self.predict(read_image, model_type, points_per_side, points_per_batch)
+        image, anns = self.predict(read_image, model_type, points_per_side, points_per_batch, min_area)
         if len(anns) == 0:
             return
 
@@ -54,7 +53,7 @@ class SegAutoMaskGenerator:
 
         return "output.jpg"
 
-    def save_video(self, source, model_type, points_per_side, points_per_batch, min_area, max_area):
+    def save_video(self, source, model_type, points_per_side, points_per_batch, min_area):
         cap, out = load_video(source)
         colors = np.random.randint(0, 255, size=(256, 3), dtype=np.uint8)
 
@@ -63,7 +62,7 @@ class SegAutoMaskGenerator:
             if not ret:
                 break
 
-            image, anns = self.predict(frame, model_type, points_per_side, points_per_batch)
+            image, anns = self.predict(frame, model_type, points_per_side, points_per_batch, min_area)
             if len(anns) == 0:
                 continue
 
@@ -73,16 +72,15 @@ class SegAutoMaskGenerator:
             )
 
             for i, ann in enumerate(sorted_anns):
-                if max_area > ann["area"] > min_area:
-                    m = ann["segmentation"]
-                    color = colors[i % 256]  # Her nesne için farklı bir renk kullan
-                    img = np.zeros((m.shape[0], m.shape[1], 3), dtype=np.uint8)
-                    img[:, :, 0] = color[0]
-                    img[:, :, 1] = color[1]
-                    img[:, :, 2] = color[2]
-                    img = cv2.bitwise_and(img, img, mask=m.astype(np.uint8))
-                    img = cv2.addWeighted(img, 0.35, np.zeros_like(img), 0.65, 0)
-                    mask_image = cv2.add(mask_image, img)
+                m = ann["segmentation"]
+                color = colors[i % 256]  # Her nesne için farklı bir renk kullan
+                img = np.zeros((m.shape[0], m.shape[1], 3), dtype=np.uint8)
+                img[:, :, 0] = color[0]
+                img[:, :, 1] = color[1]
+                img[:, :, 2] = color[2]
+                img = cv2.bitwise_and(img, img, mask=m.astype(np.uint8))
+                img = cv2.addWeighted(img, 0.35, np.zeros_like(img), 0.65, 0)
+                mask_image = cv2.add(mask_image, img)
 
             combined_mask = cv2.add(frame, mask_image)
             out.write(combined_mask)
