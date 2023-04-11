4e63462b05287213fd514778e62e5be4ab8b0cf9
kadirnar <kadir.nar@hotmail.com>
2023-04-11 21:47:20 +0300

    remove predict function

--- metaseg/utils/__init__.py
+++ metaseg/utils/__init__.py
@@ -4,5 +4,5 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-from metaseg.utils.data_utils import load_image, load_video
-from metaseg.utils.file_utils import download_model
+from metaseg.utils.data_utils import *
+from metaseg.utils.file_utils import *
--- metaseg/utils/data_utils.py
+++ metaseg/utils/data_utils.py
@@ -1,13 +1,14 @@
-import cv2
-
-
 def load_image(image_path):
+    import cv2
+
     image = cv2.imread(image_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
 
 def load_video(video_path, output_path="output.mp4"):
+    import cv2
+
     cap = cv2.VideoCapture(video_path)
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -16,3 +17,61 @@ def load_video(video_path, output_path="output.mp4"):
     out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
 
     return cap, out
+
+
+def read_image(image_path):
+    import cv2
+
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
+
+
+def load_mask(mask, random_color):
+    import numpy as np
+
+    if random_color:
+        color = np.random.rand(3) * 255
+    else:
+        color = np.array([100, 50, 0])
+
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    mask_image = mask_image.astype(np.uint8)
+    return mask_image
+
+
+def load_box(box, image):
+    import cv2
+
+    x, y, w, h = int(box[0]), int(box[1]), int(box[2]), int(box[3])
+    cv2.rectangle(image, (x, y), (w, h), (0, 255, 0), 2)
+    return image
+
+
+def plt_load_mask(mask, ax, random_color=False):
+    import numpy as np
+
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+
+def plt_load_box(box, ax):
+    import matplotlib.pyplot as plt
+
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor="green", facecolor=(0, 0, 0, 0), lw=2))
+
+
+def multi_boxes(boxes, predictor, image):
+    import torch
+
+    input_boxes = torch.tensor(boxes, device=predictor.device)
+    transformed_boxes = predictor.transform.apply_boxes_torch(input_boxes, image.shape[:2])
+    return input_boxes, transformed_boxes
