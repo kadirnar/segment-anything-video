8edeb98204405e0cd0182ac902fea18bd29d04bb
Onuralp SEZER <thunderbirdtr@gmail.com>
2023-06-27 12:51:22 +0300

    feat(examples): 🎉 video and image segautomask examples are added
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

---
+++ examples/image_segautomask.py
@@ -0,0 +1,23 @@
+from metaseg import SegAutoMaskPredictor
+
+
+# If gpu memory is not enough, reduce the points_per_side and points_per_batch.
+
+# For image
+def main(src:str="image.png") -> None:
+    results = SegAutoMaskPredictor().image_predict(
+        source=src,
+        model_type="vit_l",  # vit_l, vit_h, vit_b
+        points_per_side=16,
+        points_per_batch=64,
+        min_area=0,
+        output_path="output.jpg",
+        show=True,
+        save=False,
+    )
+
+    # print(results)
+
+
+if __name__ == "__main__":
+    main()
