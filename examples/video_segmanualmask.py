b1a2ef0b56342af3a15504fc3cd02e8d57311b28
Onuralp SEZER <thunderbirdtr@gmail.com>
2023-06-27 12:58:49 +0300

    feat(examples): 🎉 video and image segmanualmask examples are added
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- examples/video_segmanualmask.py
+++ examples/video_segmanualmask.py
@@ -1,16 +1,17 @@
-from metaseg import SegAutoMaskPredictor
+from metaseg import SegManualMaskPredictor
 
 # If gpu memory is not enough, reduce the points_per_side and points_per_batch.
 
 
-# For video
 def main(src: str = "video.mp4") -> None:
-    SegAutoMaskPredictor().video_predict(
+    SegManualMaskPredictor().video_predict(
         source=src,
         model_type="vit_l",  # vit_l, vit_h, vit_b
-        points_per_side=16,
-        points_per_batch=64,
-        min_area=1000,
+        input_point=[0, 0, 100, 100],
+        input_label=[0, 1],
+        input_box=None,
+        multimask_output=False,
+        random_color=False,
         output_path="output.mp4",
     )
 
